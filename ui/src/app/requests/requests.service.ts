77cda81c2488e7080d87b3230ecf0d98eed3f3f1
stonkchonk <heilfred98@gmail.com>
2020-11-02 19:35:04 +0100

    adding angular http client, works in browser

---
+++ ui/src/app/requests/requests.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestsService {
+
+  constructor(private http:HttpClient) { }
+
+
+  /**
+   * Sends HTTP GET requests using the angular client. For testing purposes within the browser one needs to disable CORS.
+   * For Chrome this can be done using the following plugin https://chrome.google.com/webstore/detail/cross-domain-cors/mjhpgnbimicffchbodmgfnemoghjakai/related
+   * Native HTTP client will be added
+   * @param url 
+   */
+  getRequestAngular(url:string):Promise<Object>{
+    console.log('exectute requests method')
+    return this.http.get(url).toPromise().then(
+        resp => Promise.resolve(resp)
+      ).catch(
+        err => Promise.resolve(err)
+      )
+  }
+} 
--- ui/src/app/tab1/tab1.page.ts
+++ ui/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RequestsService } from '../requests/requests.service'
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private requerster:RequestsService) {}
 
+  async test(){
+    console.log('execute test method')
+    this.requerster.getRequestAngular('https://www.google.com/').then(
+      resp => console.log('async',resp)
+    )
+    let resp = await this.requerster.getRequestAngular('https://www.google.com/')
+    console.log('sync',resp)
+  }
 }
