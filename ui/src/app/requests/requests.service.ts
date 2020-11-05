89d2f04da539e58f57561fda5385e1e64ced0978
stonkchonk <heilfred98@gmail.com>
2020-11-05 22:21:04 +0100

    added native http client

--- ui/src/app/requests/requests.service.ts
+++ ui/src/app/requests/requests.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { HTTP } from '@ionic-native/http/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestsService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private angularHttp:HttpClient, private nativeHttp: HTTP) { }
 
 
   /**
-   * Sends HTTP GET requests using the angular client. For testing purposes within the browser one needs to disable CORS.
-   * For Chrome this can be done using the following plugin https://chrome.google.com/webstore/detail/cross-domain-cors/mjhpgnbimicffchbodmgfnemoghjakai/related
-   * Native HTTP client will be added
+   * Sends HTTP GET requests using the angular client. For testing purposes within the browser one needs to disable CORS with a plugin for example.
    * @param url 
    */
-  getRequestAngular(url:string, headers):Promise<Object>{
-    console.log('exectute requests method')
-    return this.http.get(url,headers).toPromise().then(
-        resp => Promise.resolve(resp)
+  getRequestAngular(url:string):Promise<Object>{
+    return this.angularHttp.get(url).toPromise().then(
+        resp => Promise.resolve(resp) //if succesful resolve object
       ).catch(
-        err => Promise.resolve(err)
+        err => Promise.resolve(err) //if unsuccessful resolve object as well for error handling
       )
   }
+
+
+  /**
+   * Sends HTTP GET requests using the native ionic client. This works on an Android phone or within a Cordova application.
+   * It bypasses CORS policy so nothing has to be modified additionally.
+   * @param url 
+   */
+  getRequestNative(url:string):Promise<object>{
+    return this.nativeHttp.get(url,{},{}).then(
+      resp => Promise.resolve(resp) //if succesful resolve object
+    ).catch(
+      err => Promise.resolve(err) //if unsuccessful resolve object as well for error handling
+    )
+  }
+
+
 } 
+
--- ui/src/app/tab1/tab1.page.ts
+++ ui/src/app/tab1/tab1.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RequestsService } from '../requests/requests.service'
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -15,25 +14,15 @@ export class Tab1Page {
 
   async test(){
 
-    let test = new HttpHeaders({
-      'step':'900',
-      'limit':'12',
-    })
-
-    let httpOptions = {
-      //observe:'response',
-      //responseType:'json',
-      //headers:test
-    }
 
     let link1 = 'https://www.bitstamp.net/api/v2/ohlc/btceur' //ohlc
     let link2 = 'https://www.bitstamp.net/api/v2/ticker/btceur/' //ticker
     let link3 = 'https://www.bitstamp.net/api/v2/ohlc/btceur?step=900&limit=12' //ohlc+params
-    console.log('execute test method',test)
-    this.requerster.getRequestAngular(link3,httpOptions).then(
+    console.log('execute test method')
+    this.requerster.getRequestAngular(link3).then(
       resp => console.log('async',resp)
     )
-    let resp = await this.requerster.getRequestAngular(link3,httpOptions)
+    let resp = await this.requerster.getRequestAngular(link3)
     console.log('sync',resp)
   }
 }
