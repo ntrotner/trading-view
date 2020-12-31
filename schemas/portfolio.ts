ac94e51922c2c5448846559633947bd9e798e8ac
Nikita Trotner <nikita@ttnr.me>
2020-12-31 17:59:05 +0100

    added classes to position/portfolio

--- schemas/portfolio.ts
+++ schemas/portfolio.ts
@@ -1,8 +1,28 @@
 export interface portfolio {
-    positions: position[]
+  positions: position[];
+  history: Array<{ date: Date, [key: string]: number | Date }>;
 }
 
 export interface position {
-    id: string,
-    amount: number
+  id: string;
+  amount: number;
+}
+
+export class position {
+  id: string;
+  amount: number;
+
+  constructor(_id: string, _amount: number) {
+    this.id = _id;
+    this.amount = _amount;
+  }
+}
+
+export class portfolio {
+  public positions: position[];
+  public history: Array<{ date: Date, [key: string]: number | Date }> = [];
+
+  constructor(_positions?: position[]) {
+    this.positions = _positions ? _positions : [];
+  }
 }
