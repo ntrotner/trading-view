b99ce684263dfed88f7ad377b8f4a61c6dba5880
stonkchonk <heilfred98@gmail.com>
2021-02-07 02:34:55 +0100

    preparations for final features

--- schemas/timeframeOptions.ts
+++ schemas/timeframeOptions.ts
@@ -4,3 +4,18 @@ export const timeframeOptions = new Map([
     ['month',   { step:43200, limit:60 }],
     ['year',    { step:259200, limit:122 }]
 ])
+
+export const candlestickOptions = new Map([
+    ['1 min', { step:60, display:{ hour: '2-digit', minute:'2-digit' } }],
+    ['3 min', { step:180, display:{ hour: '2-digit', minute:'2-digit' } }],
+    ['5 min', { step:300, display:{ hour: '2-digit', minute:'2-digit' } }],
+    ['15 min', { step:900, display:{ hour: '2-digit', minute:'2-digit' } }],
+    ['30 min', { step:1800, display:{ hour: '2-digit', minute:'2-digit' } }],
+    ['1 hour', { step:3600, display:{ hour: '2-digit', minute:'2-digit' } }],
+    ['2 hour', { step:7200, display:{ day: '2-digit', month:'2-digit', hour:'2-digit', minute:'2-digit' } }],
+    ['4 hour', { step:14400, display:{ day: '2-digit', month:'2-digit', hour:'2-digit', minute:'2-digit' } }],
+    ['6 hour', { step:21600, display:{day: '2-digit', month:'2-digit'} }],
+    ['12 hour', { step: 43200, display:{day: '2-digit', month:'2-digit'} }],
+    ['1 day', { step: 86400, display:{day: '2-digit', month:'2-digit'} }],
+    ['3 day', { step: 259200, display:{day: '2-digit', month:'2-digit', year:'2-digit'} }]
+])
--- ui/src/app/tab1/detail/detail.page.ts
+++ ui/src/app/tab1/detail/detail.page.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router'
 import { Chart } from 'chart.js';
 import 'chartjs-plugin-labels';
 import { candlestickColors } from '../../../../../schemas/colors'
+import { candlestickOptions } from '../../../../../schemas/timeframeOptions'
 import { OhlcHistoricalDataService } from '../../services/ohlc-historical-data/ohlc-historical-data.service'
 import { LiveDataBitstampService } from '../../services/live-data-bitstamp/live-data-bitstamp.service'
 
@@ -13,33 +14,58 @@ import { LiveDataBitstampService } from '../../services/live-data-bitstamp/live-
 })
 export class DetailPage implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute, private historicalData: OhlcHistoricalDataService, private liveData: LiveDataBitstampService) { }
+  constructor(private activatedRoute: ActivatedRoute, private historicalData: OhlcHistoricalDataService, private liveData: LiveDataBitstampService) { 
+    //initialize default selected values
+    this.selectedCandleTime = candlestickOptions.get('15 min')
+
+  }
+
+  //displayable live buy sell values
+  liveTrading:{buy:number | null, sell:number | null} = {buy:null, sell:null}
+
+  // selectable variables
+  selectedFiatCurrency:string
+  selectedCandleTime:{step:number, display:object}
 
   async ngOnInit() {
     this.activatedRoute.params.subscribe(
       async data => {
         console.log(data)
-        let ohlcData = await this.historicalData.getOHLCData(data['currency'] + 'eur',900,96)
-        console.log(ohlcData)
+        let ohlcData = await this.historicalData.getOHLCData(data['currency'] + 'eur',this.selectedCandleTime.step,96)
+        console.log(ohlcData, this.totalVolume(ohlcData['data']['ohlc']))
+        console.log(this.highLow(ohlcData['data']['ohlc']))
         let openCloseBars = this._ohlcBarExtractor(ohlcData,'open','close')
         let highLowBars = this._ohlcBarExtractor(ohlcData,'high','low')
         let barColors = this._barColors(openCloseBars, candlestickColors['pump'], candlestickColors['dump'])
-        this.buildCandlestickChart(document.getElementById('candlestick'), openCloseBars, highLowBars, barColors)
+        this.buildCandlestickChart(document.getElementById('candlestick'), openCloseBars, highLowBars, barColors, this._timeLabels(ohlcData['data']['ohlc'], this.selectedCandleTime.display))
       }
     )
-    let test = this.liveData.liveSellBuy('btceur',1)
-      test.subscribe(
-        data => console.log(data),
+    
+  }
+  ionViewDidEnter(){
+    console.log('view enter')
+    this.liveData.liveSellBuy('btceur',1)
+      .subscribe(
+        data => {
+          console.log(data)
+          this.liveTrading = data
+          
+        },
         error => console.log(error),
         ()=>console.log('done')
       )
   }
 
-  buildCandlestickChart(htmlReference:HTMLElement, openCloseBars: Array<Array<any>>, highLowBars: Array<Array<any>>, barColors:Array<string>){
+  ionViewDidLeave(){
+    console.log('view leave')
+    this.liveData.unsubscribeLiveSellBuy('btceur')
+  }
+
+  buildCandlestickChart(htmlReference:HTMLElement, openCloseBars: Array<Array<any>>, highLowBars: Array<Array<any>>, barColors:Array<string>, timeLabels: Array<string>){
     var candlestickChart = new Chart(htmlReference, {
       type: 'bar',
       data: {
-        labels: barColors,
+        labels: timeLabels,
         datasets: [{ 
             data: openCloseBars,
             barThickness:4,
@@ -103,6 +129,49 @@ export class DetailPage implements OnInit {
       }
     });
     return arrayOfColors
+  }
+
+
+  /**
+   * Creates an array of time labels useable for a chart displaying ohlc data.
+   * @param ohlcData 
+   * @param displayOptions e.g. {day:'2-digit', month:'2-digit'}, basically JS-Datetime formatting.
+   */
+  _timeLabels(ohlcData:Array<object>, displayOptions:object):Array<string>{
+    let labels = []
+    ohlcData.forEach(element => {
+      let timestamp = element['timestamp']*1000
+      labels.push(new Date(timestamp).toLocaleString([], displayOptions))
+    });
+    return labels
+  }
+
+
+  /**
+   * Returns a tuple of the highest high and the lowest low in a given array.
+   * @param ohlcData 
+   */
+  highLow(ohlcData:Array<object>):[number, number]{
+    let highs = []
+    let lows = []
+    ohlcData.forEach(element => {
+      highs.push(parseFloat(element['high']))
+      lows.push(parseFloat(element['low']))
+    });
+    return [Math.max.apply(Math, highs), Math.min.apply(Math, lows)]
+  }
+
+
+  /**
+   * Returns the total trading volume in a given array.
+   * @param ohlcData 
+   */
+  totalVolume(ohlcData:Array<object>):number{
+    let totalVolume = 0
+    ohlcData.forEach(element => {
+      totalVolume = totalVolume + parseFloat(element['volume'])
+    });
+    return totalVolume
   }
 }
 
